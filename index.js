203afe05c0edcfc0bd25baf4721c2111f6a68917
Beartvgames <113101242+Beartvgames@users.noreply.github.com>
2023-10-08 03:11:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const app = express();
 // Configura el directorio donde se encuentran los archivos estáticos (por ejemplo, index.html)
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Middleware para manejar URLs limpias
+app.use((req, res, next) => {
+    if (!req.path.includes('.')) {
+        res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    } else {
+        next();
+    }
+});
+
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running on port 3000');
 });
