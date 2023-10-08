a7609c45ff1f469af0bab844c3cb105a87144616
Beartvgames <113101242+Beartvgames@users.noreply.github.com>
2023-10-08 03:31:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+
 const express = require('express');
 const path = require('path');
 const request = require('request');
 const app = express();
 const GDPSUrl = process.env.GDPS || "unknown";
+
 // Configura el directorio donde se encuentran los archivos estáticos (por ejemplo, index.html)
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -15,12 +17,13 @@ app.use((req, res, next) => {
         const apiUrl = `http://${GDPSUrl}/tools/bot/userName.php?extID=${userId}`;
         request.post(apiUrl, (error, response, body) => {
             if (!error && response.statusCode === 200) {
-                // Enviar la respuesta del otro sitio web como respuesta al cliente
-                res.send(body);
+                // Almacena la respuesta del otro sitio web en la variable res.locals.result
+                res.locals.result = body;
             } else {
                 // Manejar errores de la solicitud POST
-                res.status(500).send('Error en la solicitud POST');
+                res.locals.result = 'Error en la solicitud POST';
             }
+            next();
         });
     } else if (!req.path.includes('.')) {
         // Si no hay un parámetro 'user' y la URL no tiene una extensión, enviar index.html
@@ -33,3 +36,4 @@ app.use((req, res, next) => {
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running on port 3000');
 });
+
